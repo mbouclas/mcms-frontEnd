ef566db698012e232049fbf84d0a10d71cc440b1
Michael Bouclas <mbouclas@gmail.com>
2016-12-17 22:38:05 +0200

    Initial commit

--- src/Console/Commands/InstallerActions/SeedDataBase.php
+++ src/Console/Commands/InstallerActions/SeedDataBase.php
@@ -3,14 +3,14 @@
 namespace Mcms\FrontEnd\Console\Commands\InstallerActions;
 
 
-use Mcms\FrontEnd\Console\Commands\InstallerActions\Seeders\FormBuilder;
+use Mcms\FrontEnd\Console\Commands\InstallerActions\Seeders\FormBuilderSeed;
 use Illuminate\Console\Command;
 
 class SeedDataBase
 {
     public function handle(Command $command)
     {
-        (new FormBuilder())->handle();
+        (new FormBuilderSeed())->handle();
         $command->comment('Database seeded');
     }
 }
